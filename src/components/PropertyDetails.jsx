c64183d8ab4e726152ac70382f56b1fde28da8e5
Massimo Triassi <kidmax14@hotmail.com>
2026-01-21 20:06:06 -0500

    feat: update onMetaDataChange to be consistent with other usage

--- src/components/PropertyDetails.jsx
+++ src/components/PropertyDetails.jsx
@@ -38,7 +38,10 @@ export const PropertyDetails = ({ metadata, onMetadataChange }) => {
                                 type="text"
                                 value={metadata.centrisId}
                                 onChange={(e) =>
-                                    onMetadataChange("centrisId", e.target.value)
+                                    onMetadataChange(
+                                        "centrisId",
+                                        e.target.value,
+                                    )
                                 }
                                 placeholder="12345678"
                                 className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
